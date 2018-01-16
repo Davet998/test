696f9df81bd3b4746f6e18297fcd7c2d18bc2d81
Davet998 <dave@theapples.eu>
2018-01-16 09:01:54 +0000

    Pandas dataframes

---
+++ pandas_1.py
@@ -0,0 +1,20 @@
+# pandas
+import pandas
+df1=pandas.DataFrame([[2,4,6],[10,20,30]], columns=["Price","Age","Value"], index=["First","Second"])
+print(df1)
+print(dir(df1))
+df2=pandas.DataFrame([{"Name":"John"},{"Name":"Fred"}])
+#print(df2)
+print(df1.mean())
+print(df1.Price.max())
+print(df1.Price.min())
+
+import pandas
+#df1 = pandas.read_csv("C:\\git\\test\\supermarkets.csv")   #, header=None)
+#print(df1)
+
+#df2 = pandas.read_csv("http://pythonhow.com/supermarkets.csv")   #, header=None)
+#print(df2)
+
+df3 = pandas.read_json("http://pythonhow.com/supermarkets.json")
+print(df3)
