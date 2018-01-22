1a9361556aca8f4abd20d9fbde05385ff6b0fd50
David Thornton <dave@theapples.eu>
2018-01-22 18:56:04 +0000

    TKinter sample

--- tkinter_demo.py
+++ tkinter_demo.py
@@ -6,10 +6,15 @@ from tkinter import *
 window=Tk()
 #code goes here
 
-b1=Button(window, text="Execute")
+def km_to_miles():
+    miles = float(e1_value.get()) * 1.6
+    t1.insert(END,miles)
+
+b1=Button(window, text="Execute", command=km_to_miles)
 #b1.pack()
 b1.grid(row=0,column=0) #,rowspan=3)
-e1=Entry(window)
+e1_value = StringVar()
+e1=Entry(window, textvariable = e1_value)
 e1.grid(row=0,column=1)
 
 t1=Text(window,height=1,width=20)
