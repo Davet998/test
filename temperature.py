d3c2d8df210a2ccc868d43d871e6b0197d358465
David Thornton <dave@theapples.eu>
2018-02-13 19:32:19 +0000

    Password storage and Time functions

--- temperature.py
+++ temperature.py
@@ -1,12 +1,13 @@
 temperatures=[10,-20,-289,100]
+
 def c_to_f(c):
     if c< -273.15:
         return "That temperature doesn't make sense!"
     else:
         f=c*9/5+32
         return f
-with open("C:\\git\\test\\temps.txt",'a')as file:
 
+with open("C:\\git\\test\\temps.txt",'a')as file:
     for t in temperatures:
         print(c_to_f(t))
         file.write("\n"+str(c_to_f(t)))
---
+++ test.py
@@ -0,0 +1,9 @@
+import ttn_creds
+
+APPEUI = ttn_creds.login['APPEUI']
+APPID = ttn_creds.login['APPID']
+PSW = ttn_creds.login['PSW']
+
+print(APPEUI)
+print(APPID)
+print(PSW)
